dff779056fb19262e904514d158a2cd3a06895a8
Ekim Yazılım <ekimyazilimfe@gmail.com>
2024-09-08 16:33:56 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrtakCalismaNO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
